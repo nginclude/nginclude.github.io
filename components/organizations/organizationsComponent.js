6741abe105fca64d34c37819de5015b7130d43ec
David Silva <contacto@dvidsilva.com>
2016-05-04 15:08:30 -0600

    created organization component and dependencies

--- components/organizations/organizationsComponent.js
+++ components/organizations/organizationsComponent.js
@@ -1,12 +1,40 @@
 (function () {
   var template = [
-    '  <main class="container">',
-    '    <h1>Organizations</h1>',
+    '   <main class="container">',
+    '     <h1>Organizations</h1>',
+    '      <div class="row organization-list">',
+    '        <div class="col-sm-6 col-md-4" ng-repeat="organization in vm.list">',
+    '          <div class="thumbnail">',
+    '            <img ng-src="{{organization.logo}}" alt="{{organization.name}}">',
+    '            <div class="caption">',
+    '              <h3 ng-bind="organization.name"></h3>',
+    '              <p ng-bind="organization.short_description"></p>',
+    '              <p>',
+    '                <a ng-href="organization.url" ng-disabled="!organization.url"',
+    '                  ng-bind="organization.display_url || organization.url"></a>',
+    '              </p>',
+    '              <p class="contact" ng-if="organization.contact_name">',
+    '              <strong>Contact:</strong>',
+    '                <a ng-href="organization.contact_data" ng-disabled="!organization.contact_data"',
+    '                  ng-bind="organization.contact_name"></a>',
+    '              </p>',
+    '            </div>',
+    '          </div>',
+    '        </div>',
+    '      </div>',
     '  </main>'
   ];
   angular.module('ng-include-directory').component('organizations', {
     template: template.join(''),
+    controllerAs: 'vm',
     controller: function (Organizations) {
+      var vm = this;
+      vm.list = [];
+      vm.$onInit = function () {
+        Organizations.get().then( function (data) {
+          vm.list = data;
+        });
+      };
     }
   });
 })();
--- components/organizations/organizationsService.js
+++ components/organizations/organizationsService.js
@@ -1,7 +1,15 @@
-angular.module('ng-include-directory').factory('Organizations', function () {
+angular.module('ng-include-directory').factory('Organizations', function ($http) {
   var organizations = {};
 
-  
+  organizations.get = function () {
+    return $http.get('data/organizations.json').then( function (response) {
+      if (Array.isArray(response.data)) {
+        return response.data;
+      }
+      return [];
+    });
+  };
+
   return organizations;
 });
 
