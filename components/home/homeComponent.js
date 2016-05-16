50986f66a040f394dab34070dc446c04db86a389
Dvid Silva <dvidsilva@users.noreply.github.com>
2016-05-16 01:05:17 -0700

    added twitter to page

--- components/home/homeComponent.js
+++ components/home/homeComponent.js
@@ -1,9 +1,10 @@
 (function () {
   var template = [
     '  <main class="container">',
-    '    <h2>Challenge the status quo! <small>Making the angular community more inclusive and welcoming for underrepresented groups</small></h2>',
+    '    <h2>Challenge the status quo! <small>We aim to change the default assumption about what an Angular developer looks like.</small></h2>',
     '    <p>Site currently under construction, if you&apos;d like to collaborate join us in',
     '      <a href="https://github.com/nginclude/nginclude.github.io" target="_blank">github</a>',
+    '      and  <a href="https://twitter.com/nginclude" target="_blank">Twitter</a>.',
     '    </p>',
     '  </main>'
   ];
